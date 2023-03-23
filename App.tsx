e296195c35ff88ba5a3f7b7846ec7216426f9c9a
Mathias2860DK <mathias.enemark.poulsen@gmail.com>
2023-03-23 10:57:38 +0100

    useUser hook

--- App.tsx
+++ App.tsx
@@ -5,25 +5,13 @@ import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen'
 import { useEffect } from 'react';
 import AuthenticationStackNavigator from './src/routes/AuthenticationStackNavigator';
-import RootStackNavigator from './src/routes/RootStackNavigator';
-import { supabase } from './src/api/InitSupabse';
-import { Session } from '@supabase/supabase-js';
+import { UserContextProvider } from './src/contexts/UserContext';
 
 const App = () => {
-  const [session, setSession] = useState<Session | null>(null);
   const [fontsLoaded] = useFonts({
     'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
   });
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-  }, []);
 
   useEffect(()=> {
     async function prepare() {
@@ -31,24 +19,20 @@ const App = () => {
     }
     prepare();
   },[])
+  
 if(!fontsLoaded){
   return undefined;
 } else {
   SplashScreen.hideAsync();
 }
 
-    const renderContent = () => {
-        const isLoggedIn = session && session.user;
-
-        if (isLoggedIn) {
-            return (
-               <RootStackNavigator/>
-            );
-        }
-        return <AuthenticationStackNavigator />;
-    };
-
-    return <NavigationContainer>{renderContent()}</NavigationContainer>;
+    return (
+      <UserContextProvider>
+        <NavigationContainer>
+          <AuthenticationStackNavigator />
+      </NavigationContainer>
+    </UserContextProvider>
+    );
 };
 
 export default App;
--- src/routes/AuthenticationStackNavigator.tsx
+++ src/routes/AuthenticationStackNavigator.tsx
@@ -3,14 +3,29 @@ import LoginScreen from '../screens/LoginScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import { AuthStackParams } from './types';
 import React from 'react';
+import { useUser } from '../contexts/UserContext';
+import RootStackNavigator from './RootStackNavigator';
 
 const AuthenticationStackNavigator = () => {
   const AuthStack = createStackNavigator<AuthStackParams>();
+  const { user, session } = useUser(); 
+
+  const renderContent = () => {
+    const isLoggedIn = session && session.user;
+
+    if(!isLoggedIn){
+      return (
+        <AuthStack.Navigator>
+        <AuthStack.Screen name="Login" component={LoginScreen} />
+        <AuthStack.Screen name="SignUp" component={SignUpScreen} />
+    </AuthStack.Navigator>
+        );
+    }
+    return (<RootStackNavigator/>)
+    
+};
   return (
-      <AuthStack.Navigator>
-          <AuthStack.Screen name="Login" component={LoginScreen} />
-          <AuthStack.Screen name="SignUp" component={SignUpScreen} />
-      </AuthStack.Navigator>
+      <>{renderContent()}</>
   );
 };
 
